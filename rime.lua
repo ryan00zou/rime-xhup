bf394225cedd289b55f76207e6247acdb81898e0
ryan00zou <51985976+ryan00zou@users.noreply.github.com>
2026-08-22 09:59:27 +0800

    Add files via upload

--- rime.lua
+++ rime.lua
@@ -1,5 +1,5 @@
--- 金额大写转换
-number_translator = require("wanxiang/number_translator")
+-- 数字转换 (金额大写、科学计数、进制转换等)
+number_conversion = require("wanxiang/number_conversion")
 super_calculator = require("wanxiang/super_calculator")
 
 -- 万象日期时间功能
@@ -13,19 +13,13 @@ local super_sequence = require("wanxiang/super_sequence")
 super_sequence_processor = super_sequence.P
 super_sequence_filter = super_sequence.F
 
--- 万象快捷键调频模块
-local wanxiang = require("wanxiang/wanxiang")
-local user_predict = require("wanxiang/user_predict")
-local super_sequence = require("wanxiang/super_sequence")
-
--- 注册 user_predict 模块
-user_predict_processor = user_predict.P
-user_predict_translator = user_predict.T
-user_predict_filter = user_predict.F
-
--- 注册 super_sequence 模块
-super_sequence_processor = super_sequence.P
-super_sequence_filter = super_sequence.F
-
 -- 英文单词自动大写
 word_autocaps = require("word_autocaps")
+
+-- 手动造词编码存储
+local zaoci_phrase = require("wanxiang/zaoci_phrase")
+zaoci_phrase_filter = zaoci_phrase
+
+-- 超级提示模块
+local super_tips = require("wanxiang/super_tips")
+super_tips_processor = super_tips
